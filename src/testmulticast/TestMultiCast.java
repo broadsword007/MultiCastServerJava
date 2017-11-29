396f01fcb572191dec6ef6138a9c40cf3402464a
broadsword007 <ferhan0000@gmail.com>
2017-11-30 04:04:42 +0500

    Implemented main functionality for server inclusing a HTTP server to view the messages

--- src/testmulticast/TestMultiCast.java
+++ src/testmulticast/TestMultiCast.java
@@ -18,8 +18,17 @@ import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.SocketTimeoutException;
 import java.util.Scanner;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 /**
  *
@@ -29,21 +38,52 @@ public class TestMultiCast {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        ClientsListener c= new ClientsListener();
-        c.start();
+// TODO code application logic here
+ServerHandler h= new ServerHandler();
+h.startOperation();
     }
     
+}
+class ServerHandler
+{
+    ClientsListener listeningThread; // listens for clients on multicast channel
+    Vector<Socket> listOfClients ;
+    Vector<Message> outgoingMessageQueue;
+    GlobalSenderThread senderThread ;
+    SimpleHttpServer httpServer;
+    Vector<Message> listOfAllMessages ;
+    ServerHandler() throws IOException
+    {
+        listOfClients = new Vector<Socket>();
+        outgoingMessageQueue = new Vector<Message>();
+        listOfAllMessages = new Vector<Message>();
+        listeningThread = new ClientsListener(listOfClients, outgoingMessageQueue, listOfAllMessages);
+        senderThread = new GlobalSenderThread(listOfClients, outgoingMessageQueue);
+        httpServer = new SimpleHttpServer(listOfAllMessages);
+    }
+    public void startOperation()
+    {
+        listeningThread.start();
+        senderThread.start();
+        httpServer.start();
+    }
 }
 class ClientsListener extends Thread
 {
     protected byte[] buf = new byte[256];
     MulticastSocket listeningSocket;
-    public ClientsListener() throws IOException 
+    Vector<Message> outgoingMessageQueue;
+    Vector<Socket> listOfClients;
+    Vector<Message> listOfAllMessages ;
+    public ClientsListener(Vector<Socket> listOfClientsVal, Vector<Message> outgoingMessageQueueVal, Vector<Message> listOfAllMessagesVal) throws IOException 
     {
         listeningSocket = new MulticastSocket(4446);
+        outgoingMessageQueue = outgoingMessageQueueVal;
+        listOfAllMessages = listOfAllMessagesVal;
+        listOfClients = listOfClientsVal;
     }
     @Override
     public void run()
@@ -61,55 +101,185 @@ class ClientsListener extends Thread
                 SocketAddress sender = packet.getSocketAddress();
                 System.out.println("Message recieved from : "+sender+", Message : "+message);
                 Socket client = new Socket(packet.getAddress(), 10001);
-                System.out.println("Just connected to " + client.getRemoteSocketAddress());
-                ClientHandler handler = new ClientHandler(client);
-                handler.start();
-            }
-        }
-        catch (IOException ex) 
-        {
-            Logger.getLogger(ClientsListener.class.getName()).log(Level.SEVERE, null, ex);
-        }
+                if(client.isConnected())
+                {
+                    listOfClients.add(client);
+                    System.out.println("Just connected to " + client.getRemoteSocketAddress());
+                    ClientRecieverThread recieverThread = new ClientRecieverThread(client, listOfClients, outgoingMessageQueue, listOfAllMessages);
+                    recieverThread.start();
+                }
+                else
+                {
+                    System.out.println("Unable to connect to " + client.getRemoteSocketAddress());
+                }
+                    }
+                }
+                catch (IOException ex) 
+                {
+                    Logger.getLogger(ClientsListener.class.getName()).log(Level.SEVERE, null, ex);
+                }
     }
     
 }
-class ClientHandler extends Thread
+class GlobalSenderThread extends Thread
+{
+    Vector<Message> outgoingMessageQueue;
+    Vector<Socket> listOfClients ;
+    GlobalSenderThread(Vector<Socket> listOfClientsVal, Vector<Message> outgoingMessageQueueVal)
+    {
+        listOfClients = listOfClientsVal;
+        outgoingMessageQueue = outgoingMessageQueueVal;
+    }
+    @Override
+    public void run()
+    {
+        OutputStream outToServer;
+        try 
+        {
+            while(true)
+            {
+        if(!outgoingMessageQueue.isEmpty())
+        {
+            //send message at 0 to all clients and remove 0th index
+            String messageToSend = outgoingMessageQueue.firstElement().messageContent;
+            Socket sender = outgoingMessageQueue.firstElement().sender;
+            System.out.println("Sending message : "+messageToSend+" to all clients");
+            outgoingMessageQueue.remove(0);
+            for(int i=0; i<listOfClients.size(); i++)
+            {
+        if(!listOfClients.get(i).isClosed())
+        {
+            //if(listOfClients.get(i)!= sender) // TODO uncomment this line on release
+            {
+        DataOutputStream out = new DataOutputStream(listOfClients.get(i).getOutputStream());
+        out.writeUTF(messageToSend);
+            }
+        }
+        else
+        {
+            System.out.println("Message not sent to : "+sender.getRemoteSocketAddress()+
+            " connected was closed");
+        }
+            }
+        }
+            }
+        } 
+        catch (IOException ex) 
+        {
+            Logger.getLogger(GlobalSenderThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+class ClientRecieverThread extends Thread
 {
     Socket handlingSocket;
-    ClientHandler(Socket handlingSocketVal)
+    Vector<Message> outgoingMessageQueue;
+    Vector<Socket> listOfClients;
+    Vector<Message> listOfAllMessages ;
+    ClientRecieverThread(Socket handlingSocketVal, Vector<Socket> listOfClientsVal , Vector<Message> outgoingMessageQueueVal, Vector<Message> listOfAllMessagesVal) throws IOException
     {
         handlingSocket=handlingSocketVal;
+        outgoingMessageQueue = outgoingMessageQueueVal;
+        listOfAllMessages = listOfAllMessagesVal;
+        listOfClients = listOfClientsVal;
+        OutputStream outToServer = handlingSocket.getOutputStream();
+        DataOutputStream out = new DataOutputStream(outToServer);
+        out.writeUTF("Hi! You are hereby connected to the chat server " 
+            + handlingSocket.getLocalSocketAddress());
     }
     @Override
     public void run()
     {
         try 
         {
-            OutputStream outToServer = handlingSocket.getOutputStream();
-            DataOutputStream out = new DataOutputStream(outToServer);
             InputStream inFromServer = handlingSocket.getInputStream();
             DataInputStream in = new DataInputStream(inFromServer);
-            out.writeUTF("Hi! You are hereby connected to the chat server " 
-                    + handlingSocket.getLocalSocketAddress());
-            Scanner inputScanner = new Scanner(System.in);
             while(true)
             {
-                String serverReply= in.readUTF();
-                System.out.println("Client reply " + serverReply);
-                if(serverReply.toLowerCase().contains("disconnect"))
+                String clientReply= in.readUTF();
+                System.out.println("Client reply " + clientReply);
+                Message recievedMessage = new Message(handlingSocket, clientReply);
+                outgoingMessageQueue.add(recievedMessage);
+                listOfAllMessages.add(recievedMessage);
+                if(clientReply.toLowerCase().contains("disconnect"))
                 {
-                    out.writeUTF("Bye");
+                    listOfClients.remove(handlingSocket);
+                    System.out.println("Disconnected from : "+handlingSocket.getRemoteSocketAddress());
                     handlingSocket.close();
                     break;
                 }
-                System.out.print("Please enter a message for client : ");
-                String clientResponse= inputScanner.nextLine();
-                out.writeUTF(clientResponse);
             }
         } 
         catch (IOException ex) 
         {
-            //Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(ClientRecieverThread.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+}
+class Message
+{
+    public String messageContent;
+    Socket sender;
+    Message(Socket senderVal, String messageContentVal)
+    {
+        sender = senderVal;
+        messageContent = messageContentVal;
+    }
+}
+
+/*
+ * a simple static http server
+*/
+class SimpleHttpServer 
+{
+    HttpServer server ;
+    Vector<Message> listOfAllMessages;
+    SimpleHttpServer(Vector<Message> listOfAllMessagesVal) throws IOException 
+    {
+        listOfAllMessages = listOfAllMessagesVal;
+        server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/messages", new MyHandler(listOfAllMessages));
+        server.setExecutor(null); // creates a default executor
+    }
+    public void start()
+    {
+        server.start();
+    }
+    static class MyHandler implements HttpHandler 
+    {
+        Vector<Message> listOfAllMessages;
+        MyHandler(Vector<Message> listOfAllMessagesVal)
+        {
+            listOfAllMessages = listOfAllMessagesVal;
+        }
+        @Override
+        public void handle(HttpExchange t) throws IOException 
+        {
+            String responseStr = ("<html>\n" +
+            "    <style>\n" +
+            "    div .container\n" +
+            "    {\n" +
+            "padding-left: 10%;\n" +
+            "padding-right: 10%\n" +
+            "    }\n" +
+            "    </style>\n" +
+            "    <div class=\"container\">\n" +
+            "<h1>\n" +
+            "    Last 10 Messages\n" +
+            "</h1>\n" +
+            "</br>\n" +
+            "</br>\n" +
+            "</br>\n") ;
+            for(int i=0; i<10 && i<listOfAllMessages.size(); i++)
+            {
+                responseStr+="<p>"+listOfAllMessages.get(i).sender.getRemoteSocketAddress()+" : "
+                        +listOfAllMessages.get(i).messageContent+"</p>\n";
+            }
+            responseStr+=("</div>\n" +"</html>") ;
+            t.sendResponseHeaders(200, responseStr.getBytes().length);
+            OutputStream os = t.getResponseBody();
+            os.write(responseStr.getBytes());
+            os.close();
+      }
     }
 }
